bc0f6c462a0e72b78bfb68e37ab934609942c754
Yannick Bäumer <stix@phcn.de>
2015-01-16 12:41:33 +0100

    fixed aspect-ratio field

--- Configuration/TCA/Video.php
+++ Configuration/TCA/Video.php
@@ -6,7 +6,7 @@ if (!defined ('TYPO3_MODE')) {
 $TCA['tx_ybvideoplayer_domain_model_video'] = array (
         'ctrl' => $TCA['tx_ybvideoplayer_domain_model_video']['ctrl'],
         'interface' => array (
-                'showRecordFieldList' => 'sys_language_uid,l10n_parent,l10n_diffsource,hidden,starttime,endtime,title,description,preview,subtitle,endscreen,files, fullnameidentifier'
+                'showRecordFieldList' => 'sys_language_uid,l10n_parent,l10n_diffsource,hidden,starttime,endtime,title,description,preview,subtitle,endscreen,files,fullnameidentifier,aspectratio'
         ),
         'feInterface' => $TCA['tx_ybvideoplayer_domain_model_video']['feInterface'],
         'columns' => array (
@@ -162,7 +162,7 @@ $TCA['tx_ybvideoplayer_domain_model_video'] = array (
                 ),
 	),
        'types' => array (
-                '0' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, title;;;;2-2-2, description;;;richtext[]:rte_transform[mode=ts_css];3-3-3, preview, subtitle, files, fullnameidentifier')
+                '0' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, title;;;;2-2-2, description;;;richtext[]:rte_transform[mode=ts_css];3-3-3, preview, subtitle, files, fullnameidentifier, aspectratio')
         ),
         'palettes' => array (
                 '1' => array('showitem' => 'starttime, endtime')
