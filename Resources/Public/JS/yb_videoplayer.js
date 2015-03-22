c64a66a5665f55b7b6e19d6b11105419959cf469
Yannick Bäumer <stix@phcn.de>
2015-03-23 00:07:43 +0100

    added loading bar

--- Resources/Public/JS/yb_videoplayer.js
+++ Resources/Public/JS/yb_videoplayer.js
@@ -4,9 +4,16 @@ $(window).load(makePlayerVisible)
 function makeVideolistVisible()
 {
 	$('div.yb_videolist .videoListItem').css('visibility', '');
+	removeLoadingBars();
 }
 
 function makePlayerVisible()
 {
 	$('div.yb_videoplayer').css('visibility', '');
+	removeLoadingBars();
+}
+
+function removeLoadingBars()
+{
+	$('div.loadingBar').remove();
 }
