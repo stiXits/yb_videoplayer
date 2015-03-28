5e6f6d2f5c786ea69ec2fdeec451755cba788011
Yannick Bäumer <stix@phcn.de>
2015-03-28 14:07:19 +0100

    removed waitforimages js and added deferred image loading

--- Resources/Public/JS/yb_videoplayer.js
+++ Resources/Public/JS/yb_videoplayer.js
@@ -1,5 +1,6 @@
-$(window).load(makeVideolistVisible)
-$(window).load(makePlayerVisible)
+$(window).load(makeVideolistVisible);
+$(window).load(makePlayerVisible);
+$(window).load(loadDeferredImages);
 
 function makeVideolistVisible()
 {
@@ -16,4 +17,13 @@ function makePlayerVisible()
 function removeLoadingBars()
 {
 	$('div.loadingBar').remove();
+}
+
+function loadDeferredImages() {
+	var imgDefer = document.getElementsByTagName('img');
+	for (var i=0; i<imgDefer.length; i++) {
+		if(imgDefer[i].getAttribute('data-src')) {
+			imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+		} 
+	} 
 }
